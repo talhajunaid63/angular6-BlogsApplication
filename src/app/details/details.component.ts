b7874728967e110577aff798d331df3394778cde
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-01 19:58:25 +0500

    Fetch user details on pageload.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,14 +10,17 @@ import { ActivatedRoute } from "@angular/router";
 })
 
 export class DetailsComponent implements OnInit {
-	
-	user$: Object;
+
+  user$: Object;
 
   constructor(private route: ActivatedRoute, private data: DataService) { 
     this.route.params.subscribe( params => this.user$ = params.id );
   }
 
   ngOnInit() {
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data
+    );
   }
 
 }
