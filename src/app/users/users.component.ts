7bbfcb3982eb22e31e2d694f16091948b9cf5c40
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-01 19:28:38 +0500

    Import data service to users component.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
+
 export class UsersComponent implements OnInit {
 
   constructor() { }
