d03e48b6feac1d46ad2c712aa9f762b2c6399f26
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-01 19:32:47 +0500

    Define users object , service object and assign data to users$.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,9 +9,14 @@ import { DataService } from '../data.service';
 
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users$: Object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(
+      data => this.users$ = data
+    );
   }
 
 }
