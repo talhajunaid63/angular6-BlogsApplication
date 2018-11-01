0c4f2b6de2a53351d26565ed35343315f3dcf746
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-01 19:52:10 +0500

    Import service and observeable module to details component.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-details',
