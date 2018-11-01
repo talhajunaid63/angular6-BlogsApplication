77597ecfaec7937b3bf9e4fa6d588b8393b2922c
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-01 19:23:54 +0500

    Use HttpClient to get users from public API.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,5 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+  }
 }
